be4da79036255e26a14f9f0720fef3cbb5b457ae
the_space_warrior <daniel.kramer.j@gmail.com>
2025-02-19 19:18:44 -0600

    made screen

--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 # throughout this file
 import pygame
 from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        screen.fill(000)
+        pygame.display.flip()
+    
 
 
 if __name__ == "__main__":
