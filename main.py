1f92fae9c1e46019ced51e22882e482d9665ed3f
the_space_warrior <daniel.kramer.j@gmail.com>
2025-02-19 21:04:02 -0600

    worked on groups

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ import pygame
 from constants import *
 from player import *
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y, PLAYER_RADIUS)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(x, y, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
-        player.update(dt, PLAYER_TURN_SPEED)
-        player.draw(screen)
+        screen.fill((0, 0, 0))
+        updateable.update(dt)
+        drawable.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(6000) / 1000
 
