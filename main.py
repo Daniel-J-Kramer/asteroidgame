c64965c9585ee113930355c2c361489a4041346e
the_space_warrior <daniel.kramer.j@gmail.com>
2025-02-19 19:20:56 -0600

    added close button

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill(000)
         pygame.display.flip()
     
